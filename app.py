bffd9a30d425fa3f947c7215c34321f489b0c84a
Namav404 <sec.acc901@gmail.com>
2022-10-11 16:06:52 +0530

    Modified seat booking page

--- app.py
+++ app.py
@@ -12,12 +12,25 @@ def index():
 
 @app.route('/seat/book')
 def seat_book():
-    return render_template('seat_booking.html')
+    return render_template('seat_booking.html', seat = [3, 6, 8])
 
 
 @app.route('/admin/index')
 def admin_index():
     return render_template('admin_page.html')
+
+@app.route('/save/bus-deatils', methods = ['GET', 'POST'])
+def save_bus_details():
+    if request.method == 'POST':
+        bus_name = request.form['bus_name']
+        bus_no = request.form['bus_no']
+        bus_reg_no = request.form['bus_reg_no']
+        normal_seat_price = request.form['normal_seat_price']
+        window_seat_price = request.form['window_seat_price']
+        no_of_seats = request.form['no_of_seats']
+        start_location = request.form['start_location']
+        destination_location = request.form['destination_location']
+    return redirect(url_for('admin_index'))
 
 
 if __name__ == '__main__':
