6a4d9f3e7baad3760ed5fb74904a5809a165a173
shrithikraj10 <lio.srk10@gmail.com>
2022-10-11 16:09:29 +0530

    Stashed changes

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for,render_template,jsonify
 import os
+
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-@app.route('/')
+mysql = MySQL(app)
+
+#Enter here your database informations 
+app.config["MYSQL_HOST"] = ""
+app.config["MYSQL_USER"] = ""
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = ""
+app.config["MYSQL_CURSORCLASS"] = ""
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("base.html")
+@app.route("/livesearch",methods=["POST","GET"])
+def livesearch():
+    searchbox = request.form.get("text")
+    cursor = mysql.connection.cursor()
+    query = "select word_eng from words where word_eng LIKE '{}%' order by word_eng".format(searchbox)#This is just example query , you should replace field names with yours
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return jsonify(result)
+
+
+
+
 
 @app.route('/seat/book')
 def seat_book():
