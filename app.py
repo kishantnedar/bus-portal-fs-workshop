15e9e6297858f669dd2324c85ceea3bf7ded881f
Namav404 <sec.acc901@gmail.com>
2022-10-11 13:00:38 +0530

    Created admin portal webpage

--- app.py
+++ app.py
@@ -4,14 +4,22 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/seat/book')
 def seat_book():
     return render_template('seat_booking.html')
 
+
+@app.route('/admin/index')
+def admin_index():
+    return render_template('admin_page.html')
+
+
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5007))
-  app.run(debug = True, host = '0.0.0.0', port = port)
+    port = int(os.environ.get('PORT', 5007))
+    app.run(debug=True, host='0.0.0.0', port=port)
