9cc08c20e8739edbeac42fdeb5889807692d4d71
Nishant Kedar <nishant.kedar@infuse.it>
2022-10-12 23:57:16 +0530

    fixed add-bus page to have correct validation and db model to accomodate document id

--- controllers/admin.py
+++ controllers/admin.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from jinja2 import TemplateNotFound
-from actions.admin.bus import add_bus as add_bus_action
+from actions.admin.bus import add_bus as add_bus_action, get_busses, remove_bus as remove_bus_action
+from pymongo.errors import DuplicateKeyError
 
 admin = Blueprint('admin', __name__)
 
@@ -8,6 +9,10 @@ admin = Blueprint('admin', __name__)
 @admin.route('/add-bus', methods=['GET', 'POST'])
 def add_bus():
     if request.method == 'POST':
-        add_bus_action(bus=request.form)
-        return redirect(url_for('index'))
+        try:
+            add_bus_action(bus=request.form)
+        except DuplicateKeyError as e:
+            flash('Bus already exists')
+            return redirect(url_for('admin.add_bus'))
+        return redirect(url_for('admin.index'))
     return render_template('admin/add-bus.html')
