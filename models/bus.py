fb08e837604036b2f479ac59acbcdfcdb3760ab7
Nishant Kedar <nishant.kedar@infuse.it>
2022-10-12 12:42:44 +0530

    add bus db logic

--- models/bus.py
+++ models/bus.py
@@ -1,50 +1,39 @@
-from mongoengine import Document, StringField, IntField, DateTimeField, ListField, EmbeddedDocumentField, BooleanField, ReferenceField
 from datetime import datetime
 
 
-class Seat(Document):
-    seat_number = StringField(required=True, unique=True)
-    seat_occupied = BooleanField(required=True, default=False)
-    occupied_by = ReferenceField('User', required=False)
+class Seat:
+    def __init__(self, seat_number):
+        self.seat_number = seat_number
+        self.seat_occupied = False
+        self.reserved_by = None
 
 
-class SeatType(Document):
-    seat_price = IntField(required=True)
-    seat_count = IntField(required=True)
-    seats = ListField(EmbeddedDocumentField(Seat))
+class SeatType:
+    def __init__(self, count, price):
+        self.seat_price = price
+        self.seat_count = count
+        self.seats = [Seat(seat_number=seat_number).__dict__
+                      for seat_number in range(1, count+1)]
 
 
-class Seats(Document):
-    window_left = EmbeddedDocumentField(SeatType)
-    window_right = EmbeddedDocumentField(SeatType)
-    left = EmbeddedDocumentField(SeatType)
-    right = EmbeddedDocumentField(SeatType)
+class Seats:
+    def __init__(self, window_price, normal_price, seat_count):
+        self.window_left = SeatType(seat_count, window_price).__dict__
+        self.window_right = SeatType(seat_count, window_price).__dict__
+        self.left = SeatType(seat_count, normal_price).__dict__
+        self.right = SeatType(seat_count, normal_price).__dict__
 
 
-class Bus(Document):
-    meta = {
-        'collection': 'buses'
-    }
-    bus_number = StringField(required=True, unique=True)
-    bus_name = StringField(required=True)
-    bus_capacity = IntField(required=True)
-    bus_start = StringField(required=True)
-    bus_destination = StringField(required=True)
-    bus_seats = EmbeddedDocumentField(Seats)
-    bus_created = DateTimeField(required=True, default=datetime.now())
-    bus_updated = DateTimeField(required=True, default=datetime.now())
-
-    def __repr__(self):
-        return self.bus_name
-
-    def to_json(self):
-        return {
-            'bus_number': self.bus_number,
-            'bus_name': self.bus_name,
-            'bus_capacity': self.bus_capacity,
-            'bus_start': self.bus_start,
-            'bus_destination': self.bus_destination,
-            'bus_seats': self.bus_seats,
-            'bus_created': self.bus_created,
-            'bus_updated': self.bus_updated
-        }
+class Bus:
+    def __init__(self, bus_number, bus_name, bus_capacity, bus_start, bus_destination, bus_seats, bus_reg_number, bus_normal_seat_price, bus_window_seat_price):
+        self.bus_number = bus_number
+        self.bus_name = bus_name
+        self.bus_reg_number = bus_reg_number
+        self.bus_capacity = bus_capacity
+        self.bus_start = bus_start
+        self.bus_destination = bus_destination
+        if isinstance(bus_seats, int):
+            self.bus_seats = Seats(
+                seat_count=bus_seats, normal_price=bus_normal_seat_price, window_price=bus_window_seat_price).__dict__
+        else:
+            self.bus_seats = bus_seats
---
+++ repository/mongo.py
@@ -0,0 +1,37 @@
+from pymongo import MongoClient
+from os import environ
+
+
+class MongoRepository(object):
+    def __init__(self, mongo_collection):
+        mongoclient = MongoClient(
+            environ.get('DB_HOST'), username=environ.get('DB_UNAME'), password=environ.get('DB_PASSWD'))
+        db = mongoclient['bus-db']  # type: ignore
+        self._collection = db.mongo_collection
+
+    def insert(self, doc):
+        self._collection.insert_one(doc)
+
+    def find_one(self, query):
+        return self._collection.find(query)
+
+    def find_all(self):
+        return self._collection.find()
+
+    def update(self, query, bus):
+        return self._collection.update_one(query, bus)
+
+    def delete(self, query):
+        return self._collection.delete_one(query)
+
+    def delete_all(self):
+        return self._collection.delete_many({})
+
+    def count(self):
+        return self._collection.count_documents({})
+
+    def count_query(self, query):
+        return self._collection.count_documents(query)
+
+    def drop(self):
+        return self._collection.drop()
