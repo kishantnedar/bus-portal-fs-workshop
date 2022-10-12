0de8feac4238dc74e6ff46f5a0587af9bf75a321
Nishant Kedar <nishant.kedar@infuse.it>
2022-10-12 23:57:42 +0530

    created view for busses in system

--- controllers/admin.py
+++ controllers/admin.py
@@ -6,6 +6,14 @@ from pymongo.errors import DuplicateKeyError
 admin = Blueprint('admin', __name__)
 
 
+@admin.route('/', methods=['GET'])
+def index():
+    try:
+        return render_template('admin/index.html', buses=get_busses())
+    except TemplateNotFound:
+        return render_template('404.html'), 404
+
+
 @admin.route('/add-bus', methods=['GET', 'POST'])
 def add_bus():
     if request.method == 'POST':
@@ -16,3 +24,12 @@ def add_bus():
             return redirect(url_for('admin.add_bus'))
         return redirect(url_for('admin.index'))
     return render_template('admin/add-bus.html')
+
+
+@admin.route('/remove-bus', methods=['GET'])
+def remove_bus():
+    bus_number = request.args.get('bus_number')
+    if bus_number:
+        remove_bus_action(bus_number)
+        flash('Bus removed')
+    return redirect(url_for('admin.index'))
