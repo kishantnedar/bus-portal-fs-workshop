c3a47ddc7604a66f5efb93bdfd5895a11a644d47
shrithikraj10 <lio.srk10@gmail.com>
2022-10-13 15:21:47 +0530

    updated

--- controllers/book.py
+++ controllers/book.py
@@ -1,14 +1,21 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from actions.booking.booking import get_user_request_buses
+import pandas as pd
 
 booking = Blueprint('book', __name__)
 
 @booking.route('/search', methods=['GET', 'POST'])
 def search_bus():
-    if request.method == 'POST':
-        bus_list = get_user_request_buses(search=request.form)
-        for bus in bus_list:
-            print(bus['bus_name'])
-        return redirect(url_for('index'))
+    # if request.method == 'POST':
+    bus_list = get_user_request_buses(search=request.form)
+    #     for bus in bus_list:
+    #         print(bus['bus_name'])
+    #     return redirect(url_for('index'))
+    date = request.form['date']
+    day = pd.Timestamp(date).day_name()
+    return render_template('UserRequestbus.html', buses = bus_list,day=day)
 
-
+# @booking.route('/searched',methods=['GET', 'POST'])
+# def searched_bus():
+    
+    
