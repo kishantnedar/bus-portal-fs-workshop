b6f0e15ab2a7da6cfece0ca0ea63bde23c400655
Namav404 <sec.acc901@gmail.com>
2022-10-13 10:49:35 +0530

    New changes updated

--- actions/booking/booking.py
+++ actions/booking/booking.py
@@ -1,2 +1,10 @@
+from jmespath import search
 from models.bus import Bus
 from repository.mongo import MongoRepository
+import pandas as pd
+
+def get_user_request_buses():
+    start_location = search['from']
+    destination_location = search['to']
+    date = search['date']
+    day = pd.Timestamp(date).day_name()
--- controllers/book.py
+++ controllers/book.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from actions.booking.booking import
 
 booking = Blueprint('book', __name__)
 
