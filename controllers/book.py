bcd705bdadd39df33c5b4dc620544cbd0e12fdb7
Namav404 <sec.acc901@gmail.com>
2022-10-14 16:52:29 +0530

    Resolved issue of bus display page

--- controllers/book.py
+++ controllers/book.py
@@ -18,5 +18,5 @@ def search_bus():
 @booking.route('/seat/<int:bus_no>', methods = ['GET', 'POST'])
 def seat_book(bus_no):
     bus_details = get_selected_bus(bus_no)
-    seat_count = int(bus_details[0]['bus_capacity'] / 4)        
-    return render_template('seat_booking.html', bus = bus_details[0], seat_count = seat_count)
+    seat_count = int(bus_details['bus_capacity'] / 4)        
+    return render_template('seat_booking.html', bus = bus_details, seat_count = seat_count)
