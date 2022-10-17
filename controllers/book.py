9664956281ad10d4440f13270f489efb8b35c727
shrithikraj10 <lio.srk10@gmail.com>
2022-10-17 11:29:02 +0530

    Booking done page edited!

--- controllers/book.py
+++ controllers/book.py
@@ -1,3 +1,5 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from actions.booking.booking import get_user_request_buses, get_selected_bus
 import pandas as pd
 from actions.booking.booking import (book_ticket, get_selected_bus,
                                      get_user_request_buses, get_booking)
@@ -5,14 +7,13 @@ from flask import Blueprint, redirect, render_template, request, url_for
 
 booking = Blueprint('book', __name__)
 
-
 @booking.route('/search', methods=['GET', 'POST'])
 def search_bus():
     if request.method == 'POST':
         bus_list = get_user_request_buses(search=request.form)
         date = request.form['date']
         day = pd.Timestamp(date).day_name()
-        return render_template('UserRequestbus.html', buses=bus_list, day=day)
+        return render_template('UserRequestbus.html', buses = bus_list, day = day)
 
 
 @booking.route('/<int:bus_no>')
@@ -60,3 +61,17 @@ def confirm_booking():
 def return_to_homepage():
     if request.method == 'POST':
         return render_template('index.html')
+
+
+@booking.route('/booked/<int:bus_no>',methods=['GET', 'POST'])
+def booked(bus_no):
+    if request.method == 'POST':
+
+        bus_details = get_selected_bus(bus_no)
+        return render_template('booking_done.html',bus = bus_details)
+
+# @booking.route('/booked', methods=['GET', 'POST'])
+# def return_to_homepage():
+#     if request.method == 'POST':
+#         return render_template('index.html')
+    
