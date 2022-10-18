22f60b918e10cd5f6081f8cb0974abb2feab48e3
Nishant Kedar <nishant.kedar@infuse.it>
2022-10-18 14:14:20 +0530

    implemented bookings list

--- controllers/book.py
+++ controllers/book.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, session, url_for, flash
-from actions.booking.booking import get_user_request_buses, get_selected_bus, book_ticket, get_booking
+from actions.booking.booking import get_user_request_buses, get_selected_bus, book_ticket, get_bookings
 import pandas as pd
 from flask import Blueprint, redirect, render_template, request, url_for
 
@@ -16,7 +16,7 @@ def search_bus():
         return render_template('UserRequestbus.html', buses=bus_list, day=day)
 
 
-@booking.route('/<int:bus_no>')
+@booking.route('/bus/<int:bus_no>')
 def seat_book(bus_no):
     bus_details = get_selected_bus(bus_no)
     seat_count = int(bus_details['bus_capacity'] / 4)
@@ -35,8 +35,13 @@ def confirm_booking():
         price = request.form['price']
         book_date = session['date']
         record = {'user_id': user_id, 'bus_number': bus_number, 'window_left': window_left,
-                  'window_right': window_right, 'left': left, 'right': right, 'booked_date': book_date}
+                  'window_right': window_right, 'left': left, 'right': right, 'booked_date': book_date, 'booking_price': price}
         booking = book_ticket(record)
         session.pop('book_date', None)
         bus_details = get_selected_bus(bus_number)
-        return render_template('booking_done.html', bus=bus_details, price=price, booking=booking)
+        return render_template('booking_done.html', bus=bus_details, booking=booking)
+
+
+@booking.route('/bookings/<int:user_id>')
+def booking_list(user_id):
+    return render_template('BookingsList.html', bookinglist=get_bookings(user_id))
--- models/booking.py
+++ models/booking.py
@@ -3,10 +3,11 @@ from uuid import uuid4
 
 
 class Booking:
-    def __init__(self, booked_by, bus_number, booked_tickets, booked_date, _id=None, booked_on=None):
+    def __init__(self, booked_by, bus_number, booked_tickets, booked_date, booking_price, _id=None, booked_on=None):
         self._id = _id if _id else uuid4().hex
         self.booked_by = booked_by
         self.bus_number = bus_number
         self.booked_on = booked_on if booked_on else datetime.datetime.now()
         self.booked_tickets = booked_tickets
         self.booked_date = booked_date
+        self.booking_price = booking_price
